f075ca7bb84976c90ccccc2003a3ab681b4a92a5
fyousefi <f.yousefi2@gmail.com>
2021-05-02 05:16:06 +0430

    Get module class sfx

--- index.php
+++ index.php
@@ -30,6 +30,6 @@ if($params->get('device_type')){
   $data[0][ 'device' ] = $params->get('device_type');
 }
 
-$moduleclass_sfx = htmlspecialchars();
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
 ?>
