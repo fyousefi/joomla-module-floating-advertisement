ca398ddabfd8fee4ea1a3bffd8612a206812d750
fyousefi <f.yousefi2@gmail.com>
2022-09-15 12:16:19 +0430

    Add Copyright and symbol

--- mod_floating_advertising.php
+++ mod_floating_advertising.php
@@ -3,7 +3,7 @@
   * @package     Joomla.Site
   * @subpackage  mod_floating_advertising
   *
-  * @copyright   
+  * @copyright   Copyright (C) 
   * @license     GNU General Public License version 2 or later
   */
 
