db2ae869ec3dbc0aac374f4b065b8708e9fe369c
fyousefi <f.yousefi2@gmail.com>
2022-10-17 16:11:10 +0330

    Add assign

--- tmpl/default.php
+++ tmpl/default.php
@@ -31,7 +31,7 @@ $count = count($data);
 
 <div class="row">
     <div id="floating" class="floatingad <?php if(empty($params->get('device_type'))) echo "hidden-lg hidden-md hidden-sm hidden-xs";?>">
-         <?php foreach($params->get('device_type') as $x) {
+         <?php foreach($params->get('device_type') as $x =>) {
 
          }?>
     </div>
