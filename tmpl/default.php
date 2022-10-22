82ca095e930452b535d304e27be3d2d1316e860a
fyousefi <f.yousefi2@gmail.com>
2022-10-22 19:15:10 +0330

    Add quotations

--- tmpl/default.php
+++ tmpl/default.php
@@ -36,6 +36,6 @@ $count = count($data);
             if($x_value == 'phone') echo "visible-xs-block ";
          }?> <?php echo $moduleclass_sfx; ?>"
          <?php if($params->get('backgroundimage')): ?>
-               style="background-image:url(<?php echo $params->get();?>)">
+               style="background-image:url(<?php echo $params->get('');?>)">
     </div>
 </div>
