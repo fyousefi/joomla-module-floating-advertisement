f52b4aa3b894df7c91f00dcad8bb46dfda50f5b0
fyousefi <f.yousefi2@gmail.com>
2022-11-20 13:18:20 +0330

    Reorganized head comments

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,11 +1,11 @@
 <?php
-/**
- * @package     Joomla.Site
- * @subpackage  mod_floating_advertising
- * @author      F.Yousefi
- *
- * @copyright   Copyright (C) AsiaSun.ir All rights reserved.
- * @license     GNU General Public License version 2 or later
- */
+  /**
+  * @package     Joomla.Site
+  * @subpackage  mod_floating_advertising
+  * @author      F.Yousefi
+  *
+  * @copyright   Copyright (C) AsiaSun.ir All rights reserved.
+  * @license     GNU General Public License version 2 or later
+  */
 
 defined('_JEXEC') or die;
