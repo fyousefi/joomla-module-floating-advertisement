51d4d25a5da800a463ca9ee63139deaf1522198f
F.Yousefi <f.yousefi2@gmail.com>
2022-12-07 10:58:55 +0330

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -39,7 +39,7 @@ $count = count($data);
                style="background-image:url(<?php echo $params->get('backgroundimage');?>)"
                <?php endif; ?>>
         <div class="inner">
-             <span style="font-size:"></span>
+             <span style="font-size: 10px;"></span>
         </div>
     </div>
 </div>
