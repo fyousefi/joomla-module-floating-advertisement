cbac5e3575c9ee25528aecde6dfcd9d6d122a028
F.Yousefi <f.yousefi2@gmail.com>
2023-01-28 12:17:42 +0330

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -44,7 +44,7 @@ $count = count($data);
                 <?php if(isset($value['link']) and !empty($value['link']) ): ?>
                   <a href="<?php echo $value['link']; ?>" target="_blank" rel="nofollow">
                      <?php if(isset($value['image']) and !empty($value['image'])): ?>
-                     <img />
+                     <img class="">
                   </a>
                 <?php endif; ?>
              <?php endforeach; ?>
